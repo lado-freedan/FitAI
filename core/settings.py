c2c138a09eaaac53739f4b0b641dc33c87d828d3
Yourname <you@email.com>
2026-07-15 00:32:25 +0400

    chore: setup celery and redis containers and integrate with core Django config

--- core/settings.py
+++ core/settings.py
@@ -160,3 +160,10 @@ SIMPLE_JWT = {
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIME_ZONE = "Asia/Tbilisi"
